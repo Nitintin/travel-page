a00ebe8b744f2ce0f97eab77df4293e4e375266c
Nitin Gupta <nitin.al.gupta@accenture.com>
2021-07-31 17:48:32 +0530

    refactoring sass files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './Style.sass'
+import './Styles/Style.sass'
 
 ReactDOM.render(
   <React.StrictMode>
